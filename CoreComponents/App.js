f4304db3f98f7cabfcf0dca6d413678d45777fe9
Kamila <kamilaingridc@gmail.com>
2024-02-20 16:46:58 -0300

    ?

---
+++ CoreComponents/App.js
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+
+export default function App(){
+  return <View style={{ flex: 1, backgroundColor: 'plum', padding: 60}}>
+    <Text>Hello World</Text>
+  </View>
+}
+
